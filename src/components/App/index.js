6154ca2a814c25c23845baea80c3192ed8861b55
sent1nel101 <37023270+sent1nel101@users.noreply.github.com>
2021-11-03 13:55:39 -0400

    added list items

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,24 +1,23 @@
-import './App.css';
 import React, { Component } from 'react';
-import Intro from '../Intro/intro-component.js';
+import Intro from '../Intro/intro-component';
+import Series from '../../containers/series/index.js'
+import './App.css';
+import 'whatwg-fetch';
 
 
 
-class App extends Component {
-  state = {
-    myArray: []
-  }
+class App extends Component { 
   render(){
-  return (
-    <div className="App">
-      <header className="App-header">     
-      <h1 className="App-title">Learning React-JS</h1>
-      </header>
-      <Intro message="This the new message."/>
-      The length of our Array is {this.state.myArray.length}
-    </div>
-  );
-}
+    return (
+      <div className="App">
+        <header className="App-header">     
+        <h1 className="App-title">TV Show App</h1>
+        </header>
+        <Intro message="Find all your favorite TV shows!"/>
+        <Series />
+      </div>
+    );
+  }
 }
 
 export default App;
